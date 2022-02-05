6e43753bd5d39d7df0d927bbb33b5342830592d9
elsiezhu <elsie.zhu2003@gmail.com>
2022-02-05 15:35:23 -0500

    fixed deletion error

--- shopgreen/src/App.js
+++ shopgreen/src/App.js
@@ -2,9 +2,19 @@ import React, { useEffect, useState } from "react";
 import './App.css';
 
 function App() {
+    const [test, setTest] = useState(0);
+
+    useEffect(() => {
+       console.log('fetch')
+       fetch('test').then(res => res.json()).then(data => {
+         setTest(data.test);
+       });
+     }, []);
+
   return (
     <div className="App">
       <h1>Hello</h1>
+      <p>Welcome. This number is a test! {test}</p>
     </div>
   );
 }
